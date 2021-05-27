afe2cada7a58a287309e8b907ecb4800ce9e1377
bharatmaska <bharatmaska163@gmail.com>
2021-05-28 03:03:51 +0530

    init factory

--- js/app.js
+++ js/app.js
@@ -6,6 +6,37 @@
     $routeProvider.when("/", {
       controller: "HomeController",
       templateUrl: "views/home.html",
+      // resolve: {
+      //   productTypes: [
+      //     "AP_Districts",
+      //     function (initializeDistrictsOfAP) {
+      //       return initializeDistrictsOfAP.get();
+      //     },
+      //   ],
+      // },
     });
   });
+
+  app.factory("initializeDistrictsOfAP", [
+    "$http",
+    "$q",
+    "$filter",
+    function ($http, $q, $filter) {
+      return {
+        get: function () {
+          return $http({
+            method: "GET",
+            url: "https://cdn-api.co-vin.in/api/v2/admin/location/districts/2",
+          }).then(
+            function (response) {
+              if (response.data) return response.data;
+            },
+            function (response) {
+              alert(response.message);
+            }
+          );
+        },
+      };
+    },
+  ]);
 })();
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,43 +3,47 @@
 
   angular
     .module("myApp")
-    .controller("HomeController", function ($scope, $http, $filter) {
-      "use strict";
+    .controller(
+      "HomeController",
+      function ($scope, $http, $filter, AP_Districts) {
+        "use strict";
 
-      const TODAY = $filter("date")(Date.now(), "dd-MM-yyyy");
+        const TODAY = $filter("date")(Date.now(), "dd-MM-yyyy");
 
-      $scope.vaccineCenters = [];
-      $scope.selectedDistrict = {};
-      $scope.showText = false;
+        $scope.vaccineCenters = [];
+        $scope.selectedDistrict = {};
+        $scope.showText = false;
+        // $scope.districts = AP_Districts;
 
-      fetch("https://cdn-api.co-vin.in/api/v2/admin/location/districts/2")
-        .then((x) => x.json())
-        .then((d) => ($scope.districts = d.districts));
+        fetch("https://cdn-api.co-vin.in/api/v2/admin/location/districts/2")
+          .then((x) => x.json())
+          .then((d) => ($scope.districts = d.districts));
 
-      function getVaccineCentersByDistrict(id) {
-        $http
-          .get(
-            "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" +
-              id +
-              "&date=" +
-              TODAY
-          )
-          .then((d) => {
-            if (d.data.centers && d.data.centers.length) {
-              d.data.centers.forEach((ctr) => {
-                ctr.sessions.forEach((session) => {
-                  session.slots = session.slots.join("\n");
+        function getVaccineCentersByDistrict(id) {
+          $http
+            .get(
+              "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" +
+                id +
+                "&date=" +
+                TODAY
+            )
+            .then((d) => {
+              if (d.data.centers && d.data.centers.length) {
+                d.data.centers.forEach((ctr) => {
+                  ctr.sessions.forEach((session) => {
+                    session.slots = session.slots.join("\n");
+                  });
                 });
-              });
-              $scope.vaccineCenters = d.data.centers || [];
-            }
-          });
-      }
+                $scope.vaccineCenters = d.data.centers || [];
+              }
+            });
+        }
 
-      $scope.listCentersByDistrict = function (id) {
-        const districtChoosen = JSON.parse(id);
-        $scope.showText = districtChoosen.district_name;
-        getVaccineCentersByDistrict(districtChoosen.district_id);
-      };
-    });
+        $scope.listCentersByDistrict = function (id) {
+          const districtChoosen = JSON.parse(id);
+          $scope.showText = districtChoosen.district_name;
+          getVaccineCentersByDistrict(districtChoosen.district_id);
+        };
+      }
+    );
 })();
